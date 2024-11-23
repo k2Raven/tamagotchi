6cc7a95d93cd22b8e5d92b506a46c1008b91d8ca
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2024-11-23 19:42:38 +0600

    реализовали главную страницу

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+cat = {
+    'name': None,
+    'age': 1,
+    'satiety': 40,
+    'happiness': 40,
+}
+
+def index(request):
+    if cat['name']:
+        print(cat)
+
+    if request.method == "GET":
+        return render(request, 'index.html')
+    elif request.method == "POST":
+        name = request.POST.get('name')
+        if name:
+            cat['name'] = name
+        return render(request, 'index.html')
